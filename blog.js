8ef4b1a4bcf97c74090871dd5a417c952c76b634
magaduzela <magaduzelankosi@gmail.com>
2021-05-19 12:27:47 +0200

    update for blog project

--- blog.js
+++ blog.js
@@ -1,1 +1,29 @@
-location.href = "./aboutPage.html";
+/*location.href = "./contact.html";*/
+
+function submit() {
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var mnumber = document.getElementById('mnumber').value;
+    var email = document.getElementById('email').value;
+    var province = document.getElementById('province').value;
+    var subject = document.getElementById('subject').value;
+    if (fname == "" && lname == "" && mnumber == "" && email == "" && province == "" && subject == "") {
+        alert("Name must be filled out");
+
+    } else {
+        contactForm();
+
+
+    }
+}
+
+function contactForm() {
+    var formData = {};
+    formData["fname"] = document.getElementById('fname').value;
+    formData["lname"] = document.getElementById('lname').value;
+    formData["mnumber"] = document.getElementById('mnumber').value;
+    formData["email"] = document.getElementById('email').value;
+    formData["province"] = document.getElementById('province').value;
+    formData["subject"] = document.getElementById('subject').value;
+    return formData;
+}
